0a7ddcae2184631d93c2bfdbf0ca9626a09f801d
Max Rafiandy <max.rafiandy@gmail.com>
2020-10-11 18:57:04 +0700

    minimize logging methods

--- logger.go
+++ logger.go
@@ -1,99 +1,83 @@
 package handler
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"time"
 )
 
-var (
-	buf        bytes.Buffer
-	loggerInfo = log.New(&buf, "INFO: ", log.Lshortfile)
+const (
+	levelERROR string = "ERROR"
+	levelINFO  string = "INFO"
 )
 
 // Logger logs information to log/info.log file
-func Logger(info interface{}) {
-	// check parent type of info interface
+func Logger(payload interface{}) {
+	// check parent type of payload interface
 
 	createLogDirectory()
 
-	switch info.(type) {
+	switch payload.(type) {
 	// if it's a response
 	case Response:
 		// get data's data type
-		data := info.(Response)
+		data := payload.(Response)
 
 		// check whatever the Data is an error
 		switch data.Data.(type) {
 		// the response contain error
 		case error, Error, *Error:
-			logError(info)
+			writeLog(levelERROR, payload)
 
 		// the response is a map
-		case map[string]interface{}:
-			logInfo(info)
+		// case map[string]interface{}:
+		// 	logInfo(payload)
 
 		// it's a pure response
 		default:
-			logResponse(info)
+			writeLog(levelINFO, payload)
 		}
 
 	// if its an error
 	case error, Error, *Error:
-		logError(info)
+		writeLog(levelERROR, payload)
 
 	// otherwise return it as default response
 	default:
-		logInfo(info)
+		writeLog(levelINFO, payload)
 	}
 }
 
-// logError logs information to log/error.log file
-func logError(info interface{}) {
-	now := time.Now().Format(formatDateYMD)
-	filepath := fmt.Sprintf("log/ERROR_%s.log", now)
-	file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
+// writeLog logs information to log/info.log file
+func writeLog(level string, payload interface{}) {
+	var (
+		err      error
+		now      string
+		filepath string
+		file     *os.File
+	)
+
+	if level == "" {
+		level = "INFO"
+	}
+
+	now = time.Now().Format(formatDateYMD)
+	filepath = fmt.Sprintf("log/%s_%s.log", level, now)
+
+	if file, err = os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666); err != nil {
 		log.Fatalf("[go-handler] error opening file: %v", err)
 	}
 	defer file.Close()
 
 	log.SetOutput(file)
-	log.Println(info)
-}
 
-// logInfo logs information to log/error.log file
-func logInfo(info interface{}) {
-	now := time.Now().Format(formatDateYMD)
-	filepath := fmt.Sprintf("log/DEBUG_%s.log", now)
-	file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("[go-handler] error opening file: %v", err)
-	}
-	defer file.Close()
-
-	log.SetOutput(file)
-	log.Println(info)
-}
-
-// logResponse logs information to log/info.log file
-func logResponse(info interface{}) {
-	now := time.Now().Format(formatDateYMD)
-	filepath := fmt.Sprintf("log/RESPONSE_%s.log", now)
-	file, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("[go-handler] error opening file: %v", err)
-	}
-	defer file.Close()
-
-	log.SetOutput(file)
-	log.Println(info)
+	log.Printf("[go-handler] %s: %+v\n", level, payload)
 }
 
 func createLogDirectory() {
-	if _, err := os.Stat("log"); os.IsNotExist(err) {
+	var err error
+	if _, err = os.Stat("log"); os.IsNotExist(err) {
 		if err = os.Mkdir("log", os.ModeDir); err != nil {
 			log.Fatalf("[go-handler] fatal: %v", err)
 		}
